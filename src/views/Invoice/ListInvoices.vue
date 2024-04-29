64070ab2a0bd2d13e4cd1cab3d5528caa7b21630
Ludovit Scholtz <ludko2@gmail.com>
2024-04-29 23:31:33 +0200

    new invoice update

--- src/views/Invoice/ListInvoices.vue
+++ src/views/Invoice/ListInvoices.vue
@@ -5,12 +5,16 @@ import Column from 'primevue/column'
 import Button from 'primevue/button'
 import Card from 'primevue/card'
 import InputText from 'primevue/inputtext'
+import InputNumber from 'primevue/inputnumber'
+import Calendar from 'primevue/calendar'
+import Dropdown from 'primevue/dropdown'
+
 import { FilterMatchMode } from 'primevue/api'
 import { ref, onMounted, watch, reactive } from 'vue'
 //import { InvoiceService } from '@/service/InvoiceService'
 import { useAppStore } from '@/stores/app'
 const store = useAppStore()
-import { bffGetInvoices } from '@/scripts/axios/BFF'
+import { bffGetContacts, bffGetInvoices } from '@/scripts/axios/BFF'
 
 interface IPaymentMethod {
   currency: string
@@ -55,9 +59,9 @@ const state = reactive({
       items: [] as IItem[],
       personIdIssuer: '',
       personIdReceiver: '',
-      dateIssue: '',
-      dateDue: '',
-      dateDelivery: '',
+      dateIssue: new Date(),
+      dateDue: new Date(),
+      dateDelivery: new Date(),
       noteBeforeItems: '',
       noteAfterItems: '',
       currency: ''
@@ -65,7 +69,10 @@ const state = reactive({
   },
   filters: {
     global: { value: null, matchMode: FilterMatchMode.CONTAINS }
-  }
+  },
+  contacts: [] as any[],
+  issuer: {} as any,
+  receiver: {} as any
 })
 
 async function loadInvoices(offset: number, limit: number) {
@@ -76,6 +83,14 @@ async function loadInvoices(offset: number, limit: number) {
   invoices.value = await bffGetInvoices(store.state.authState.arc14Header, offset, limit)
 }
 
+async function loadContacts() {
+  if (!store.state.authState?.arc14Header) {
+    console.log('NotAuthorization')
+    return
+  }
+  state.contacts = await bffGetContacts(store.state.authState.arc14Header)
+}
+
 watch(store.state.authState, async () => {
   await loadInvoices(0, 10)
 })
@@ -83,27 +98,215 @@ watch(store.state.authState, async () => {
 onMounted(async () => {
   //InvoiceService.getInvoicesMini().then((data) => (products.value = data))
   await loadInvoices(0, 10)
+  await loadContacts()
 })
 
 const showForm = () => {
   state.formShown = true
+
+  const today = new Date()
+  today.setHours(0, 0, 0, 0)
+  state.selection.data.dateIssue = today
+  state.selection.data.dateDelivery = today
+  const datePlus14 = new Date()
+  datePlus14.setDate(new Date().getDate() + 14)
+
+  state.selection.data.dateDue = datePlus14
 }
 
 const cancel = () => {
   state.formShown = false
 }
+
+watch(
+  () => state.selection.data.personIdIssuer,
+  () => {
+    state.issuer = state.contacts.find((c) => c.id == state.selection.data.personIdIssuer)
+  }
+)
+watch(
+  () => state.selection.data.personIdReceiver,
+  () => {
+    state.receiver = state.contacts.find((c) => c.id == state.selection.data.personIdReceiver)
+  }
+)
+
 const invoices = ref()
 </script>
 <template>
   <Layout :hideTopMenu="false">
     <Card>
       <template #content>
+        {{ state.selection.data }}
         <div v-if="state.formShown" class="grid">
-          <div class="col">
+          <div class="col-6">
             <h1>Create new invoice</h1>
           </div>
-          <div class="col text-right">
+          <div class="col-6 text-right">
             <Button @click="cancel" :disabled="state.processing">Cancel</Button>
+          </div>
+
+          <div class="col-6 grid">
+            <div class="col-12">
+              <label for="invoiceNumberNum" class="mr-1 w-10rem inline-block">Invoice #</label>
+              <InputNumber
+                input-id="invoiceNumberNum"
+                v-model="state.selection.data.invoiceNumberNum"
+              ></InputNumber>
+            </div>
+
+            <div class="col-6 grid">
+              <div class="col-12">
+                <label for="personIdIssuer" class="mr-1 w-10rem inline-block">From:</label>
+                <Dropdown
+                  input-id="personIdIssuer"
+                  v-model="state.selection.data.personIdIssuer"
+                  :options="state.contacts"
+                  optionLabel="data.businessName"
+                  optionValue="id"
+                ></Dropdown>
+              </div>
+              <div v-if="state.issuer && state.issuer.data">
+                <div class="col-12" v-if="state.issuer.data.businessName">
+                  <label class="mr-1 w-10rem inline-block">businessName:</label>
+                  <span>{{ state.issuer.data.businessName }}</span>
+                </div>
+                <div class="col-12" v-if="state.issuer.data.companyId">
+                  <label class="mr-1 w-10rem inline-block">companyId:</label>
+                  <span>{{ state.issuer.data.companyId }}</span>
+                </div>
+                <div class="col-12" v-if="state.issuer.data.companyTaxId">
+                  <label class="mr-1 w-10rem inline-block">companyTaxId:</label>
+                  <span>{{ state.issuer.data.companyTaxId }}</span>
+                </div>
+                <div class="col-12" v-if="state.issuer.data.companyVATId">
+                  <label class="mr-1 w-10rem inline-block">companyVATId:</label>
+                  <span>{{ state.issuer.data.companyVATId }}</span>
+                </div>
+                <div class="col-12" v-if="state.issuer.data.firstName">
+                  <label class="mr-1 w-10rem inline-block">firstName:</label>
+                  <span>{{ state.issuer.data.firstName }}</span>
+                </div>
+                <div class="col-12" v-if="state.issuer.data.lastName">
+                  <label class="mr-1 w-10rem inline-block">lastName:</label>
+                  <span>{{ state.issuer.data.lastName }}</span>
+                </div>
+                <div class="col-12" v-if="state.issuer.data.email">
+                  <label class="mr-1 w-10rem inline-block">email:</label>
+                  <span>{{ state.issuer.data.email }}</span>
+                </div>
+                <div class="col-12" v-if="state.issuer.data.phone">
+                  <label class="mr-1 w-10rem inline-block">phone:</label>
+                  <span>{{ state.issuer.data.phone }}</span>
+                </div>
+                <div
+                  class="col-12"
+                  v-if="state.issuer.data.address && state.issuer.data.address.street"
+                >
+                  <label class="mr-1 w-10rem inline-block">address:</label>
+                  <span
+                    >{{ state.issuer.data.address.street }}
+                    {{ state.issuer.data.address.streetLine2 }}
+                    {{ state.issuer.data.address.zipCode }} {{ state.issuer.data.address.city }}
+                    {{ state.issuer.data.address.countryCode }}</span
+                  >
+                </div>
+              </div>
+            </div>
+            <div class="col-6 grid">
+              <div class="col-12">
+                <label for="personIdReceiver" class="mr-1 w-10rem inline-block">To:</label>
+                <Dropdown
+                  input-id="personIdReceiver"
+                  v-model="state.selection.data.personIdReceiver"
+                  :options="state.contacts"
+                  optionLabel="data.businessName"
+                  optionValue="id"
+                ></Dropdown>
+              </div>
+              <div v-if="state.receiver && state.receiver.data">
+                <div class="col-12" v-if="state.receiver.data.businessName">
+                  <label class="mr-1 w-10rem inline-block">businessName:</label>
+                  <span>{{ state.receiver.data.businessName }}</span>
+                </div>
+                <div class="col-12" v-if="state.receiver.data.companyId">
+                  <label class="mr-1 w-10rem inline-block">companyId:</label>
+                  <span>{{ state.receiver.data.companyId }}</span>
+                </div>
+                <div class="col-12" v-if="state.receiver.data.companyTaxId">
+                  <label class="mr-1 w-10rem inline-block">companyTaxId:</label>
+                  <span>{{ state.receiver.data.companyTaxId }}</span>
+                </div>
+                <div class="col-12" v-if="state.receiver.data.companyVATId">
+                  <label class="mr-1 w-10rem inline-block">companyVATId:</label>
+                  <span>{{ state.receiver.data.companyVATId }}</span>
+                </div>
+                <div class="col-12" v-if="state.receiver.data.firstName">
+                  <label class="mr-1 w-10rem inline-block">firstName:</label>
+                  <span>{{ state.receiver.data.firstName }}</span>
+                </div>
+                <div class="col-12" v-if="state.receiver.data.lastName">
+                  <label class="mr-1 w-10rem inline-block">lastName:</label>
+                  <span>{{ state.receiver.data.lastName }}</span>
+                </div>
+                <div class="col-12" v-if="state.receiver.data.email">
+                  <label class="mr-1 w-10rem inline-block">email:</label>
+                  <span>{{ state.receiver.data.email }}</span>
+                </div>
+                <div class="col-12" v-if="state.receiver.data.phone">
+                  <label class="mr-1 w-10rem inline-block">phone:</label>
+                  <span>{{ state.receiver.data.phone }}</span>
+                </div>
+                <div
+                  class="col-12"
+                  v-if="state.receiver.data.address && state.receiver.data.address.street"
+                >
+                  <label class="mr-1 w-10rem inline-block">address:</label>
+                  <span
+                    >{{ state.receiver.data.address.street }}
+                    {{ state.receiver.data.address.streetLine2 }}
+                    {{ state.receiver.data.address.zipCode }}
+                    {{ state.receiver.data.address.city }}
+                    {{ state.receiver.data.address.countryCode }}</span
+                  >
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-6 grid">
+            <div class="col-12 text-right">
+              <label for="dateIssue" class="mr-1">Issued on</label>
+              <Calendar
+                inputId="dateIssue"
+                dateFormat="dd/mm/yy"
+                v-model="state.selection.data.dateIssue"
+                showIcon
+                showButtonBar
+                :showOnFocus="false"
+              />
+            </div>
+            <div class="col-12 text-right">
+              <label for="dateDue" class="mr-1">Due date</label>
+              <Calendar
+                inputId="dateDue"
+                dateFormat="dd/mm/yy"
+                v-model="state.selection.data.dateDue"
+                showIcon
+                showButtonBar
+                :showOnFocus="false"
+              />
+            </div>
+            <div class="col-12 text-right">
+              <label for="dateDelivery" class="mr-1">Delivery date</label>
+              <Calendar
+                inputId="dateDelivery"
+                dateFormat="dd/mm/yy"
+                v-model="state.selection.data.dateDelivery"
+                showIcon
+                showButtonBar
+                :showOnFocus="false"
+              />
+            </div>
           </div>
         </div>
         <div v-else>
